10f28f7608ebaf92e2a9ab609db74ea18f9f5e19
Aleksander Veksler <aleksve@gmail.com>
2024-11-10 18:52:56 +0100

    init

---
+++ example.py
@@ -0,0 +1,31 @@
+import time
+from datetime import timedelta
+from functools import partial
+
+from periodic_tasks import periodic, run_loop, make_periodic
+
+start_time = time.time()
+
+@periodic(interval=0.1)
+def task_1():
+    dt = time.time() - start_time
+    print(f"Started a _fast_ task at t={dt:.3f}")
+
+@periodic(interval=0.5)
+def task_2():
+    dt = time.time() - start_time
+    print(f"Started a *slow* task at t={dt:.3f}")
+
+    if dt < 2:
+        time.sleep(0.91)
+    else:
+        time.sleep(0.09)
+
+def task_3(custom_text: str):
+    print(custom_text)
+    time.sleep(0.5)
+
+
+make_periodic(partial(task_3, "Hello periodic"), interval=timedelta(milliseconds=100))
+
+run_loop()
